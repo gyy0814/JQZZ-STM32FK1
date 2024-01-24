be698c1d576e262d9a96645ce30cc31dd12e5f7b
GVNDDD <131772567+GVNDDD@users.noreply.github.com>
2024-01-24 19:41:08 +0800

    2024/01/24匣女程序创建

--- Core/Inc/user.h
+++ Core/Inc/user.h
@@ -21,10 +21,34 @@ void GameInit(void);
 
 #define TO_BIT(num) (1<<(num%32))
 
-#define 关门检测 0
-#define 语音识别1 1
-#define 语音识别2 2
-#define 钥匙开门 3
+#define 入口门锁 0
+#define 电视信号1 1
+#define 电视信号2 2
+#define 电视信号3 3
+#define 电视信号4 4
+#define 电视信号5 5
+#define 电视信号6 6
+#define 接电话信号 7
+#define 卧室门锁  8
+#define 卧室柜锁 9
+#define 敲门锁 10
+#define 卧室柜下灯 11
+#define 电视信号4 12
+#define 电视信号5 13
+#define 电视信号6 14
+#define 接电话信号 15
+
+
+
+
+
+
+
+
+
+
+
+
 
 #define 门锁 2
 #define 语音识别电源 3
